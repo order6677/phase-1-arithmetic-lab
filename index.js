bb37cd689aaf46d456a8bd171f91bf1a580a2320
awadh <awadh.hassan@student.moringaschool.com>
2022-08-16 14:12:08 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
+ let num1 = 31;
+ let num2 = 2;
+ let multiply = num1 * num2;
 
-// Write your code here
+ console.log (multiply);
+
+ let num3 = 400;
+ let num4 = 44;
+ let mod = num3 % num4;
+ console.log (mod);
+
+
+ const random = Math.floor(Math.random() * 10) + 1 ;
+
+console.log(random);
+
+
+
+const max = Math.max(11,12,20);
+
+
+console.log(max);
+
+
+
